cded3cad9ddd9e70df576629a84e30474b5b5d7e
gemmerson03 <gemmerson03@gmail.com>
2024-11-10 18:41:25 -0500

    Update GameManager.cs to include coin

--- GameManager.cs
+++ GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
     public GameObject enemy;
+    public GameObject coin;
     public GameObject cloud;
     private int score;
 
@@ -20,6 +21,7 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateCoin", 2f, 6f);
         CreateSky();
         score = 0;
         scoreText.text = "Score: " + score;
@@ -36,7 +38,12 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
     }
-
+    
+ void CreateCoin()
+ {
+     Instantiate(coin,new Vector3(Random.Range(-9f, 9f), 7.5f, 0),Quaternion.identity);
+ }
+ 
     void CreateSky()
     {
         for (int i = 0; i < 30; i++)
