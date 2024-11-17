14e7b2dfe955323b439caffba26ca8e1d38ef960
MDStory97 <matt@matthewstory.com>
2024-11-16 22:08:51 -0500

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -8,25 +8,27 @@ public class Player : MonoBehaviour
 
     private float horizontalInput;
     private float verticalInput;
+    private float horizontalScreenSize = 11.5f;
+    private float verticalScreenSize = 7.5f;
     private float speed;
-    private float horizontalScreenLimit;
-    private float verticalScreenLimit;
+    private int shooting;
+    private bool hasShield;
 
-    public GameObject coin;
-    public GameObject explosion;
     public GameObject bullet;
+    public GameObject explosion;
+    public GameObject thruster;
+    public GameObject shield;
     private int lives;
 
-    private GameManager gameManager; // Reference to GameManager
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 6f;
-        horizontalScreenLimit = 11.5f;
-        verticalScreenLimit = 7.5f;
         lives = 3;
-
+        shooting = 1;
+        hasShield = false;
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         gameManager.UpdateLives(lives); // Initialize lives display
     }
@@ -42,12 +44,12 @@ public class Player : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
-        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+        transform.Translate(new Vector3(horizontalInput, verticalInput,0) * Time.deltaTime * speed);
+        if (transform.position.x > horizontalScreenSize || transform.position.x <= -horizontalScreenSize)
         {
             transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
-        if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
+        if (transform.position.y > verticalScreenSize || transform.position.y < -verticalScreenSize)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
         }
@@ -55,21 +57,103 @@ public class Player : MonoBehaviour
 
     void Shooting()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject newBullet = Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
-            newBullet.tag = "Weapon"; // Set the tag to "Weapon"
+            switch (shooting)
+            {
+                case 1:
+                    Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(bullet, transform.position + new Vector3(-0.5f, 1, 0), Quaternion.identity);
+                    Instantiate(bullet, transform.position + new Vector3(0.5f, 1, 0), Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(bullet, transform.position + new Vector3(-0.5f, 1, 0), Quaternion.Euler(0, 0, 30f)); 
+                    Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+                    Instantiate(bullet, transform.position + new Vector3(0.5f, 1, 0), Quaternion.Euler(0, 0, -30f));
+                    break;
+            }
         }
     }
 
     public void LoseALife()
     {
-       lives--;
-       gameManager.UpdateLives(lives); // Update the UI lives counter
-       if (lives <= 0)
+        if (hasShield == false)
         {
+            lives--;
+        } else if (hasShield == true)
+        {
+            //lose the shield
+            //no longer have a shield
+        }
+
+        if (lives == 0)
+        {
+            gameManager.GameOver();
             Instantiate(explosion, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
+        }
+    }
+
+    private IEnumerator SpeedPowerDown() 
+    {
+        yield return new WaitForSeconds(3f);
+        speed = 6f;
+        thruster.gameObject.SetActive(false);
+        gameManager.UpdatePowerupText("");
+    }
+
+    IEnumerator ShootingPowerDown()
+    {
+        yield return new WaitForSeconds(3f);
+        shooting = 1;
+        gameManager.UpdatePowerupText("");
+    }
+
+    IEnumerator ShieldPowerDown()
+    {
+        yield return new WaitForSeconds(6f); // Shield lasts for 6 seconds
+        shield.gameObject.SetActive(false); // Deactivate the shield
+        gameManager.UpdatePowerupText(""); // Reset the powerup text
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatIHit)
+    {
+        if(whatIHit.tag == "Powerup")
+        {
+            gameManager.PlayPowerUp();
+            int powerupType = Random.Range(1, 5); //this can be 1, 2, 3, or 4
+            switch(powerupType)
+            {
+                case 1:
+                    //speed powerup
+                    speed = 9f; // This activates the speed powerup
+                    gameManager.UpdatePowerupText("Picked up Speed!");
+                    thruster.gameObject.SetActive(true);
+                    StartCoroutine(SpeedPowerDown());
+                    break;
+                case 2:
+                    //double shot
+                    shooting = 2; // This activates the double shot
+                    gameManager.UpdatePowerupText("Picked up Double Shot!");
+                    StartCoroutine (ShootingPowerDown());
+                    break;
+                case 3:
+                    //triple shot
+                    shooting = 3; // This activates the triple shot
+                    gameManager.UpdatePowerupText("Picked up Triple Shot!");
+                    StartCoroutine(ShootingPowerDown());
+                    break;
+                case 4:
+                    //shield
+                    gameManager.UpdatePowerupText("Picked up Shield!");
+                    hasShield = true; // This activates the shield
+                    shield.gameObject.SetActive(true); // This will make the shield appear
+                    StartCoroutine(ShieldPowerDown());
+                    break;
+            }
+            Destroy(whatIHit.gameObject); // Destroy the powerup object after pickup
         }
     }
 }
